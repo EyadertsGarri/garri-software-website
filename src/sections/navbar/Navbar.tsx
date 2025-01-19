a10da568d8455ae3bc6be399ed47fb0cf0d75f35
EyadertsGarri <eyader@garrilogistics.com>
2025-01-19 23:04:03 +0300

    finish navbar component

--- src/sections/navbar/Navbar.tsx
+++ src/sections/navbar/Navbar.tsx
@@ -17,23 +17,24 @@ export function NavBar() {
     >
       <div className="flex items-center justify-between py-6">
         <button onClick={() => scrollToSection('home')} className="flex items-center space-x-2">
-          <div className="w-8 h-8 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 rounded-full" />
-          <span className="text-xl font-bold">G-SW</span>
+          {/* <div className="w-8 h-8 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 rounded-full" />
+          <span className="text-xl font-bold">G-SW</span> */}
+          <img src="/logo/garrilg-logo.png" alt="logo" className="max-w-[130px]" />
         </button>
         
-        <div className="hidden md:flex items-center space-x-8">
+        <div className="hidden md:flex items-center space-x-8 ">
           {navigationItems.map((item:NavItem) => (
             <button
               key={item.label}
               onClick={() => scrollToSection(item.sectionId)}
-              className="text-sm font-medium text-gray-600 hover:text-gray-900 transition-colors"
+              className="text-blue-600 font-gilroyRegular font-medium  text-lg hover:text-blue-900 transition-colors"
             >
               {item.label}
             </button>
           ))}
         </div>
 
-        <Button variant="default" className="bg-black text-white hover:bg-black/90">
+        <Button onClick={() => scrollToSection('contactUs')} variant="default" className="bg-gradient-to-br from-purple-800  to-blue-500 hover:bg-blue-600 text-white">
           <Phone className="mr-2 h-4 w-4" />
           Contact Us
         </Button>
--- src/sections/navbar/navbar-data.ts
+++ src/sections/navbar/navbar-data.ts
@@ -2,7 +2,9 @@ import { NavItem } from './navbar-types'
 
 export const navigationItems: NavItem[] = [
   { label: 'Home', href: '#home', sectionId: 'home' },
-  { label: 'Solutions', href: '#solutions', sectionId: 'solutions' },
   { label: 'About Us', href: '#about', sectionId: 'about' },
-  { label: 'Resources', href: '#resources', sectionId: 'resources' },
+  { label: 'Products', href: '#products', sectionId: 'products' },
+  { label: 'Our Teams', href: '#teams', sectionId: 'teams' },
+  { label: 'Testimonials', href: '#testimonials', sectionId: 'testimonials' },
+
 ]

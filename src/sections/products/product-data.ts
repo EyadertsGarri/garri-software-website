1a0040856b6935d8f5cd079de21905b4a6e7305a
EyadertsGarri <eyader@garrilogistics.com>
2025-01-25 10:54:10 +0300

    change product data

--- src/sections/products/product-data.ts
+++ src/sections/products/product-data.ts
@@ -1,29 +1,49 @@
 import { Product } from './product-types'
 
 export const products: Product[] = [
+  // {
+  //   id: 1,
+  //   title: "HR and Payroll System",
+  //   description: "Simplify your HR processes and manage payroll efficiently with our all-in-one HR and Payroll software.",
+  //   imageUrl: "/images/product-hr.png",
+  //   ctaPrimary: {
+  //     text: "Start Free Trial",
+  //     href: "/hr-trial"
+  //   },
+  //   ctaSecondary: {
+  //      text: "Explore More",
+  //     href: "/hr-payroll"
+  //   },
+  //   functionalities: [
+  //     "Employee self-service portal",
+  //     "Automated payroll calculations",
+  //     "Time and attendance tracking",
+  //     "Compliance management and reporting"
+  //   ]
+  // },
   {
     id: 1,
-    title: "HR and Payroll System",
-    description: "Simplify your HR processes and manage payroll efficiently with our all-in-one HR and Payroll software.",
-    imageUrl: "/images/product-hr.png",
+    title: " Garri Shipper TMS",
+    description: "Optimize your logistics and streamline operations with our comprehensive transport management solution.",
+    imageUrl: "/images/product-tms.png",
     ctaPrimary: {
-      text: "Start Free Trial",
-      href: "/hr-trial"
+      text: "Request a Demo",
+      href: "/transport-demo"
     },
     ctaSecondary: {
-       text: "Explore More",
-      href: "/hr-payroll"
+      text: "Explore More",
+      href: "/transport-management"
     },
     functionalities: [
-      "Employee self-service portal",
-      "Automated payroll calculations",
-      "Time and attendance tracking",
-      "Compliance management and reporting"
+      "Real-time tracking of shipments",
+      "Route optimization and planning",
+      "Automated invoicing and billing",
+      "Integration with third-party logistics"
     ]
   },
   {
     id: 2,
-    title: "Transport Management System",
+    title: "Garri Dispatch TMS",
     description: "Optimize your logistics and streamline operations with our comprehensive transport management solution.",
     imageUrl: "/images/product-tms.png",
     ctaPrimary: {
